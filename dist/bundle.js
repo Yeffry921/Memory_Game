8c920232fffc8b6a7728c0db3ab94527c6da279e
yeffry <yeffry45@gmail.com>
2020-11-25 17:17:11 -0500

    Initial setup files commit

---
+++ dist/bundle.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("const h1 = document.createElement('h1');\r\nh1.textContent = 'Initial Project Setup'\r\n\r\ndocument.querySelector('#root').appendChild(h1);\n\n//# sourceURL=webpack://mem_game/./src/index.js?");
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const h1 = document.createElement('h1');
+h1.textContent = 'Initial Project Setup'
+
+document.querySelector('#root').appendChild(h1);
